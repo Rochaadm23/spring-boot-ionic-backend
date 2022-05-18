40fe6f547c97ade1f4b4b66d7c0116289cfdc8ad
Fernando Rocha <ribeiro.ribeiro821@gmail.com>
2022-05-18 16:23:36 -0300

    Testando a primeira classe de dominio(categoria)

--- src/main/java/com/rsecinformation/cursomc/entities/Categoria.java
+++ src/main/java/com/rsecinformation/cursomc/entities/Categoria.java
@@ -1,4 +1,49 @@
 package com.rsecinformation.cursomc.entities;
 
-public class Categoria {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/rsecinformation/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rsecinformation/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,25 @@
 package com.rsecinformation.cursomc.resources;
 
+import com.rsecinformation.cursomc.entities.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String print(){
-        return "Hello world!";
+    public List<Categoria> print(){
+        Categoria c1 = new Categoria(1, "Informática");
+        Categoria c2 = new Categoria(2, "Escritório");
+
+        List<Categoria> list = new ArrayList<>();
+        list.add(c1);
+        list.add(c2);
+        return list;
     }
 }
