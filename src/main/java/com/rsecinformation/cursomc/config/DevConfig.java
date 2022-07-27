4e135f43a06a61a007c7799fcd50407dd2f5bda3
Fernando Rocha <ribeiro.ribeiro821@gmail.com>
2022-07-27 14:59:07 -0300

    Ajuste Heroku2

--- src/main/java/com/rsecinformation/cursomc/config/DevConfig.java
+++ src/main/java/com/rsecinformation/cursomc/config/DevConfig.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 import java.text.ParseException;
@@ -34,7 +33,6 @@ public class DevConfig {
     }
 
     @Bean
-    @Primary
     public EmailService emailService() {
         return new SmtpEmailService();
     }
